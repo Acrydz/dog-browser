4d4a4378ebeaeadd596b7888d803788fe0aa29ea
Darcy Almanza <darcy.almanza@gmail.com>
2023-03-03 18:34:13 -0500

    calling dog-api for breed-list

--- src/pages/breed-list.jsx
+++ src/pages/breed-list.jsx
@@ -1,18 +1,42 @@
 import TextField from "@mui/material/TextField";
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
-import Box from '@mui/material/Box';
+import Box from "@mui/material/Box";
+import { useEffect, useState } from "react";
+import axios from "axios";
 export function BreedList() {
+  const [breeds, setBreeds] = useState([]);
+  useEffect(() => {
+    axios.get("https://dog.ceo/api/breeds/list/all").then((response) => {
+      setBreeds(Object.keys(response.data.message));
+    });
+  }, []);
+
+  const [search, setSearch] = useState("");
+  const filterBreeds = search
+    ? breeds.filter((b) => b.indexOf(search) == 0)
+    : breeds;
+
   return (
-   <Box sx={{ p: 2 }}>
-    <Grid container spacing={2}>
-      <Grid item xs={12}>
-        <TextField id="outlined-basic" label="Filter Breeds" variant="outlined" />
+    <Box sx={{ p: 2 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <TextField
+            id="outlined-basic"
+            label="Filter Breeds"
+            variant="outlined"
+            value={search}
+            onChange={(e) => {
+              setSearch(e.target.value);
+            }}
+          />
+        </Grid>
+        {filterBreeds.map((b) => (
+          <Grid key={b} item xs={3}>
+            <Button variant="contained">{b}</Button>
+          </Grid>
+        ))}
       </Grid>
-      <Grid item xs={4}>
-        <Button variant="contained">xs=4</Button>
-      </Grid>
-    </Grid>
-   </Box> 
+    </Box>
   );
 }
