4869967e70a3cb3ef0343e81265fbe2235b9b853
Darcy Almanza <darcy.almanza@gmail.com>
2023-03-03 18:00:55 -0500

    breed page structure

--- src/App.js
+++ src/App.js
@@ -3,10 +3,36 @@ import { BreedList } from './pages/breed-list';
 import { BreedDetail } from './pages/breed-detail';
 import { SubBreedDetail } from './pages/sub-breed-detail';
 import { Routes, Route } from "react-router-dom";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function App() {
   return (
     <>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          
+        </Toolbar>
+      </AppBar>
+    </Box>
     <Routes>
       <Route path="/" element={<BreedList />} />
       <Route path="breed" element={<BreedDetail />} />
--- src/pages/breed-list.jsx
+++ src/pages/breed-list.jsx
@@ -1,3 +1,18 @@
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import Box from '@mui/material/Box';
 export function BreedList() {
-    return <div>Breeds</div>;
+  return (
+   <Box sx={{ p: 2 }}>
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <TextField id="outlined-basic" label="Filter Breeds" variant="outlined" />
+      </Grid>
+      <Grid item xs={4}>
+        <Button variant="contained">xs=4</Button>
+      </Grid>
+    </Grid>
+   </Box> 
+  );
 }
